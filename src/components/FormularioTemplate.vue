3e14bc00e5486d6a9fdeae3bb483469b4eed8f2f
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-08-23 14:01:54 -0400

    Variables enlazadas.

--- src/components/FormularioTemplate.vue
+++ src/components/FormularioTemplate.vue
@@ -1,44 +1,60 @@
 <template>
   <form>
     <label for="titulo">Título: </label>
-    <input id="titulo" type="text">
+    <input id="titulo" type="text" v-model="titulo">
     <br><hr>
     <label for="emoji">Emoji: </label>
-    <select name="emoji" id="emoji">
+    <select name="emoji" id="emoji" v-model="emoji">
       <option value="(ᓀ‸ᓂ)">(ᓀ‸ᓂ)</option>
       <option value="ᓀ‸ᓂ">ᓀ‸ᓂ</option>
     </select>
     <br><hr>
     <label >Apreciación: </label>
-    <input type="radio" name="apreciacion" id="Regular" value="Regular">
+    <input type="radio" name="apreciacion" id="Regular" value="Regular" v-model="apreciacion">
     <label for="Regular">Regular</label>
-    <input type="radio" name="apreciacion" id="Bien" value="Bien">
+    <input type="radio" name="apreciacion" id="Bien" value="Bien" v-model="apreciacion">
     <label for="Bien">Bien</label>
-    <input type="radio" name="apreciacion" id="Muy bien" value="Muy bien">
+    <input type="radio" name="apreciacion" id="Muy bien" value="Muy bien" v-model="apreciacion">
     <label for="Muy bien">Muy bien</label>
-    <input type="radio" name="apreciacion" id="Increible" value="Increible">
+    <input type="radio" name="apreciacion" id="Increible" value="Increible" v-model="apreciacion">
     <label for="Increible">Increible</label>
     <br><hr>
     <label >Competencias aprendidas: </label>
-    <input type="checkbox" name="competencias" id="morfologia" value="Morfología de un componente">
+    <input type="checkbox" name="competencias" id="morfologia" value="Morfología de un componente" v-model="competencias">
     <label for="morfologia">Morfología de un componente</label>
-    <input type="checkbox" name="competencias" id="directivas" value="Directivas">
+    <input type="checkbox" name="competencias" id="directivas" value="Directivas" v-model="competencias">
     <label for="directivas">Directivas</label>
-    <input type="checkbox" name="competencias" id="binding" value="One-Way y Two-Way data binding">
+    <input type="checkbox" name="competencias" id="binding" value="One-Way y Two-Way data binding" v-model="competencias">
     <label for="binding">One-Way y Two-Way data binding</label>
-    <input type="checkbox" name="competencias" id="estado" value="Estado">
+    <input type="checkbox" name="competencias" id="estado" value="Estado" v-model="competencias">
     <label for="estado">Estado</label>
-    <input type="checkbox" name="competencias" id="mvvm" value="El patrón de diseño MVVM">
+    <input type="checkbox" name="competencias" id="mvvm" value="El patrón de diseño MVVM" v-model="competencias">
     <label for="mvvm">El patrón de diseño MVVM</label>
     <br><hr>
     <label for="opinion">Opinión: </label>
-    <textarea name="opinion" id="opinion" cols="30" rows="10"></textarea>
+    <textarea name="opinion" id="opinion" cols="30" rows="10" v-model="opinion"></textarea>
   </form>
+  <article>
+    <h1>{{ emoji }}</h1>
+    <h2>{{ titulo }}</h2>
+    <h3>Y me parece {{ apreciacion }}</h3>
+    <h4>He aprendido:<br>{{ competencias.join(", ") }}</h4>
+    <h5>Mi opinión hasta ahora del framework es:<br>{{ opinion }}</h5>
+  </article>
   </template>
 
 <script>
 export default {
   name: 'FormularioTemplate',
+  data(){
+    return {
+      titulo: "",
+      emoji: "",
+      apreciacion: "",
+      competencias: [],
+      opinion: "",
+    }
+  }
 }
 </script>
 
